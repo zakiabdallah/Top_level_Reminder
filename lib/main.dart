8f15a16776729f0ce6474bee4e8d219d674395c8
zakiabdallah <yousfatezaki00@gmail.com>
2022-07-17 07:05:25 +0100

    print

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("h");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: "/",
